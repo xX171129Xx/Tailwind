0787d50df2a8c60ff96602213128aa7b1030dde8
Samson Orr <171129@student.vansd.org>
2023-02-14 23:44:39 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.js',
+    './postcss.config.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
